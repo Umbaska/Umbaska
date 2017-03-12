c37f57dc0c434cdb5d5d32b26efd10014fb0a7c7
xXAndrew28Xx <andrewtran312@gmail.com>
2017-03-12 18:31:45 -0500

    Add wrapper methods to get stuff from Skript's ParseResult.

--- src/main/java/uk/co/umbaska/skript/ParseResult.java
+++ src/main/java/uk/co/umbaska/skript/ParseResult.java
@@ -1,6 +1,13 @@
 package uk.co.umbaska.skript;
 
+import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
+import uk.co.umbaska.registrations.UExpression;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.regex.MatchResult;
 
 /**
  * @author Andrew Tran
@@ -12,5 +19,23 @@ public class ParseResult {
         this.parseResult = parseResult;
     }
 
+    public String getExpr(){
+        return parseResult.expr;
+    }
 
+    public UExpression<?>[] getExpressions(){
+        return Arrays.stream(parseResult.exprs).map((Function<Expression<?>, ? extends UExpression<?>>) UExpression::new).toArray(UExpression[]::new);
+    }
+
+    public int getMark(){
+        return parseResult.mark;
+    }
+
+    public List<MatchResult> getMatchResults(){
+        return parseResult.regexes;
+    }
+
+    public MatchResult getMatchResult(int index){
+        return getMatchResults().get(index);
+    }
 }
